101ea3c098a95c42a516fc5c709a92273a4dcc59
Andrew <shpuntik0902@gmail.com>
2021-09-16 18:37:02 +0300

    Migrate usersList and usersRepo to RxJava

--- GBAndroidLibrariesLes2/app/src/main/java/com/example/gb_libs_lesson1/presentation/UsersPresenter.kt
+++ GBAndroidLibrariesLes2/app/src/main/java/com/example/gb_libs_lesson1/presentation/UsersPresenter.kt
@@ -1,10 +1,13 @@
 package com.example.gb_libs_lesson1.presentation
 
+import android.util.Log
 import com.example.gb_libs_lesson1.model.GithubUser
 import com.example.gb_libs_lesson1.model.GithubUsersRepo
 import com.example.gb_libs_lesson1.screens.AndroidScreens
 import com.example.gb_libs_lesson1.view.UserItemView
 import com.example.gb_libs_lesson1.view.ui.users.UsersView
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
@@ -33,7 +36,7 @@ class UsersPresenter(
         super.onFirstViewAttach()
 
         viewState.init()
-        loadData()
+        observeGithubUsers()
 
         usersListPresenter.itemClickListener = { itemView ->
             val githubUser = usersListPresenter.users[itemView.pos]
@@ -45,14 +48,37 @@ class UsersPresenter(
         router.navigateTo(AndroidScreens.UserDetailScreen(argument))
     }
 
-    fun loadData() {
-        val users = usersRepo.getUsers()
-        usersListPresenter.users.addAll(users)
-        viewState.updateList()
+    private fun observeGithubUsers() {
+        val usersObservable = usersRepo.getUsersObservable()
+        usersObservable.subscribe(usersObserver)
     }
 
     fun backPressed(): Boolean {
+        usersObserver.disposable?.dispose()
         router.exit()
         return true
+    }
+
+    private val usersObserver = object : Observer<GithubUser> {
+        var disposable: Disposable? = null
+
+        private val TAG = "UsersObserver"
+        override fun onNext(user: GithubUser) {
+            usersListPresenter.users.add(user)
+            viewState.updateList()
+        }
+
+        override fun onError(e: Throwable) {
+            Log.d(TAG, "Error")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            Log.d(TAG, "Subscribe")
+            disposable = d
+        }
+
+        override fun onComplete() {
+            Log.d(TAG, "onComplete")
+        }
     }
 }
