a03e6ac24f37f0d60be504d19e936bea76d94703
Andrew <shpuntik0902@gmail.com>
2021-09-20 22:47:08 +0300

    Added subscribeOn and observeOn

--- GBAndroidLibrariesLes4/app/src/main/java/com/pascal/sample/presentation/MainPresenter.kt
+++ GBAndroidLibrariesLes4/app/src/main/java/com/pascal/sample/presentation/MainPresenter.kt
@@ -2,6 +2,8 @@ package com.pascal.sample.presentation
 
 import com.pascal.sample.domain.FileName
 import com.pascal.sample.domain.interactors.StorageInteractors
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import moxy.MvpPresenter
 
 /**
@@ -19,6 +21,8 @@ class MainPresenter(private val storageInteractors: StorageInteractors) : MvpPre
         storageInteractors
             .cacheFileFromAssets
             .execute(FileName.flowerFile)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
                     viewState.showToast("File initialization done successfully!")
@@ -33,6 +37,8 @@ class MainPresenter(private val storageInteractors: StorageInteractors) : MvpPre
         storageInteractors
             .convertFileToPngAndSave
             .execute(fileName)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
                     viewState.showToast("File converted successfully!")
