c7e7e84668a7a291bd3e7fe18338b8953d165dd2
Andrew <shpuntik0902@gmail.com>
2021-10-06 21:18:23 +0300

    Added Cicerone dagger module

---
+++ Rick&Morty-Archive/app/src/main/java/com/pascal/rma/presentation/navigation/AppScreens.kt
@@ -0,0 +1,39 @@
+package com.pascal.rma.presentation.navigation
+
+import com.pascal.rma.presentation.screens.categories.CategoriesFragment
+import com.pascal.rma.presentation.screens.character.list.CharactersFragment
+import ru.terrakok.cicerone.android.support.SupportAppScreen
+
+/**
+ * Created by dronpascal on 05.10.2021.
+ */
+object AppScreens {
+
+//    class CategoriesScreen : SupportAppScreen() {
+//        override fun getFragment() = CategoriesFragment()
+//    }
+//
+//    class CharactersScreen() : SupportAppScreen() {
+//        override fun getFragment() = CharactersFragment()
+//    }
+//
+//    class CharacterDetailScreen(private val character: Character) : SupportAppScreen() {
+//        override fun getFragment() = CharacterDetailFragment.newInstance(character)
+//    }
+//
+//    class LocationsScreen() : SupportAppScreen() {
+//        override fun getFragment() = LocationsFragment
+//    }
+//
+//    class LocationDetailScreen(private val character: Location) : SupportAppScreen() {
+//        override fun getFragment() = LocationDetailFragment.newInstance(character)
+//    }
+//
+//    class EpisodesScreen() : SupportAppScreen() {
+//        override fun getFragment() = EpisodesFragment
+//    }
+//
+//    class EpisodeDetailScreen(private val character: Episode) : SupportAppScreen() {
+//        override fun getFragment() = EpisodeDetailFragment.newInstance(character)
+//    }
+}
---
+++ Rick&Morty-Archive/app/src/main/java/com/pascal/rma/presentation/navigation/BackButtonListener.kt
@@ -0,0 +1,9 @@
+package com.pascal.rma.presentation.navigation
+
+/**
+ * Created by dronpascal on 05.10.2021.
+ */
+interface BackButtonListener {
+
+    fun backPressed(): Boolean
+}
