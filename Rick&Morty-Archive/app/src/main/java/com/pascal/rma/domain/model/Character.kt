1acf45a92d53614193db6fbea29ff01b9f80252e
Andrew <shpuntik0902@gmail.com>
2021-10-12 18:53:50 +0300

    Added GetCharacters use-case

--- Rick&Morty-Archive/app/src/main/java/com/pascal/rma/domain/model/Character.kt
+++ Rick&Morty-Archive/app/src/main/java/com/pascal/rma/domain/model/Character.kt
@@ -4,16 +4,21 @@ package com.pascal.rma.domain.model
  * Created by dronpascal on 05.10.2021.
  */
 data class Character(
-    val created: String,
-    val episode: List<String>,
-    val gender: String,
     val id: Int,
-    val image: String,
-    val location: List<String>,
     val name: String,
-    val origin: String,
-    val species: String,
     val status: String,
+    val species: String,
     val type: String,
-    val url: String
-)
+    val gender: String,
+    val image: String,
+    val origin: String,
+    val originId: Int,
+    val location: String,
+    val locationId: Int,
+    val episodes: List<Episode>
+) {
+    data class Episode(
+        val id: Int,
+        val name: String
+    )
+}
