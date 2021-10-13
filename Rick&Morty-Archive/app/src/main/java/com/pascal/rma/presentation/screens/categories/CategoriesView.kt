ff9c12944887435eaa70fd05eb6dbef7c46d39ea
Andrew <shpuntik0902@gmail.com>
2021-10-13 23:13:59 +0300

    Updated Categories presentation

--- Rick&Morty-Archive/app/src/main/java/com/pascal/rma/presentation/screens/categories/CategoriesView.kt
+++ Rick&Morty-Archive/app/src/main/java/com/pascal/rma/presentation/screens/categories/CategoriesView.kt
@@ -2,6 +2,7 @@ package com.pascal.rma.presentation.screens.categories
 
 import moxy.MvpView
 import moxy.viewstate.strategy.alias.AddToEndSingle
+import moxy.viewstate.strategy.alias.OneExecution
 
 /**
  * Created by dronpascal on 05.10.2021.
@@ -10,4 +11,7 @@ interface CategoriesView : MvpView {
 
     @AddToEndSingle
     fun initView()
+
+    @OneExecution
+    fun showSnackbar(message: String)
 }
